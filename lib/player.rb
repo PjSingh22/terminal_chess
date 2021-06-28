ab8da5c2a45580659e5ccd7c7921616ccebb8418
Prabhjot <pjsingh1359@gmail.com>
2021-06-28 15:06:34 -0700

    add translate input

--- lib/player.rb
+++ lib/player.rb
@@ -6,9 +6,35 @@ class Player
   end
 
   def get_pos
-    gets
-      .chomp
-      .split(",")
-      .map {|part| part.to_i}
+    input = gets.chomp
+    puts input
+    translate_input(input)
+  end
+
+  def translate_input(input)
+    split_input = input.split('')
+    x = 8 - split_input.last.to_i
+    y = case split_input.first.downcase
+        when 'a'
+          0
+        when 'b'
+          1
+        when 'c'
+          2
+        when 'd'
+          3
+        when 'e'
+          4
+        when 'f'
+          5
+        when 'g'
+          6
+        when 'h'
+          7
+        else
+          puts 'input does not work'
+        end
+
+    [x, y]
   end
 end
